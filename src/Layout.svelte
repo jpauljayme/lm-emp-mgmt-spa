46448300fa190a80117f5b1086091ddf5ef38b5d
John Paul Jayme <jpaul.jayme@gmail.com>
2024-08-26 18:21:11 +0800

    [feature/view-employee] Implemented reusable form for update. Fixed update-employee's contacts and address.

---
+++ src/Layout.svelte
@@ -0,0 +1,46 @@
+<!-- src/Layout.svelte -->
+<script>
+    import { currentPage } from './stores/employeeStore';
+</script>
+
+<div class="grid__wrapper">
+    <header class="grid__header">
+        <div class="header__title">
+            <h1 class="mt-5">LM Employee Dashboard</h1>
+        </div>
+        <div class="header__logout">
+            <button type="button" class="btn btn-danger">Logout</button>
+        </div>
+    </header>
+    
+    <main>
+        <svelte:component this={$currentPage}/>
+    </main>
+</div>
+
+<style>
+        .grid__wrapper {
+        display: grid;
+        grid-template-rows: max-content max-content;
+        grid-template-areas:
+            "header"
+            "employees";
+        gap: 20px;
+        padding: 20px;
+    }
+
+    .grid__header {
+        grid-area: header;
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
+        padding: 10px;
+        border-bottom: 1px solid #dee2e6;
+    }
+
+    .header__title h1 {
+        font-size: 1.5rem;
+        font-weight: bold;
+        margin: 0;
+    }
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,6 @@
 import page from 'page';
 import { writable } from 'svelte/store';
-
-// Store to keep track of the current page component
-export const currentPage = writable(null);
+import { currentAction, currentEmployeeId, currentPage } from './stores/employeeStore';
 
 // Import Svelte components
 // import Login from './pages/Login.svelte';
@@ -12,12 +10,20 @@ import EmployeeForm from './pages/EmployeeForm.svelte';
 
 // Define routes
 // page('/login', () => currentPage.set(Login));
-page('', () => currentPage.set(Dashboard));
-page('/employeeForm', () => currentPage.set(EmployeeForm));
-// page('/employee/:id', (context) => {
-//   currentPage.set(EmployeeDetail);
-//   // Optionally, use another store for route parameters if needed
-// });
+page('/', () => currentPage.set(Dashboard));
+// page('/employeeForm', () => currentPage.set(EmployeeForm));
+page('/employeeForm', (context) => {
+    const queryParams = new URLSearchParams(context.querystring);
+    const action = queryParams.get('action');
+    const id = queryParams.get('id');
+    
+    console.log(action);
+    
+    currentAction.set(action || 'add');
+    currentEmployeeId.set(id || null);
+    
+    currentPage.set(EmployeeForm);
+});
 
 // Start the router
 page();
